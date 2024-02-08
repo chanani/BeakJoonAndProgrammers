88ede60511dcab5b2a6f6845d44613ebcee445b7
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-02-08 12:36:20 +0900

    [Bronze I] Title: 세로읽기, Time: 208 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String[][] arr =  new String[15][15];
+        for(int i = 0; i < 5; i++){
+            String[] str = scan.next().split("");
+            for(int j = 0; j < str.length; j++){
+                arr[i][j] = str[j];
+            }
+        }
+
+        String answer = "";
+        for(int i = 0; i < 15; i++){
+            for(int j = 0; j < 15; j++){
+                if(arr[j][i] != null){
+                    answer += arr[j][i];
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
