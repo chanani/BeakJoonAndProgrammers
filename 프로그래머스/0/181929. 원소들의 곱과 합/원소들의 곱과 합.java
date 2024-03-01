7755430a024fc60f1381ecb9dbe4d4c0e635ef2c
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-03-02 00:15:22 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 1;
+        int sum = 0;
+        for(int i = 0; i < num_list.length; i++){
+            answer *= num_list[i];
+            sum += num_list[i];
+        }
+        return answer < sum * sum ? 1 : 0;
+    }
+}
