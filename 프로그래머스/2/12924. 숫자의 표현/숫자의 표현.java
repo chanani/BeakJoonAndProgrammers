30a2f436ad02138ebf2d35c6d14f51da3fb40a05
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-03-09 19:39:51 +0900

    [level 2] Title: 숫자의 표현, Time: 0.90 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        
         for(int i = 1; i <= n; i++){
             int sum = i;
             int rt = i + 1;
@@ -11,7 +10,6 @@ class Solution {
             }
             if(sum == n) answer++;
         }
-        
         return answer;
     }
 }
