8487d4409dde5c9ea9cbb0b65227480c6e6258ae
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-03-13 19:24:43 +0900

    [level 1] Title: 소수 찾기, Time: 14.78 ms, Memory: 55.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        int[] check = new int[n + 1];
+        
+        for(int i = 2; i <= n; i++){
+            if(check[i] == 0){
+                answer++;
+                for(int j = i; j <= n; j += i){
+                    check[j] = 1;
+                }
+            }
+        }
+        
+        
+        return answer;
+    }
+}
