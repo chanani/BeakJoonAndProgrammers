1f96dfc5a44f68b66c9d2fd69fe30d3ae8fa1f0a
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-03-28 13:54:55 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.12 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        myString = myString.replaceAll("A", "C").replaceAll("B", "A").replaceAll("C", "B");
+        if(myString.contains(pat)) answer = 1;
+        return answer;
+    }
+}
