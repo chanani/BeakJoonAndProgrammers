5d8c96d9f06bb18f058174bd4c4a23eb31433ab2
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-04-06 23:38:11 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 2.68 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        String[] str = my_string.toLowerCase().split("");
+        Arrays.sort(str);
+        for(int i = 0; i < str.length; i++) answer += str[i];
+        return answer;
+    }
+}
