2118b28ba2d497f7dbf8abf91b6632d313e73a5d
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-07-31 19:06:44 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 53.49 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,10 +1,9 @@
 class Solution {
     public String solution(int n) {
-        String answer = "";
-        for(int i = 0; i < n; i++){
-            if(i % 2 == 0) answer += "수";
-            else answer += "박";
-        }
-        return answer;
+        String str = "";
+       for(int i = 1; i <= n; i++){
+           str += i % 2 != 0 ? "수" : "박";
+       }
+       return str;
     }
 }
