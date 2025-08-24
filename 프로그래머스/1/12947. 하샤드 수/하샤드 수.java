e6ea38bce3f8adf89b47207ec0d950af6d542de2
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-08-24 22:37:30 +0900

    [level 1] Title: 하샤드 수, Time: 1.68 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,12 +1,6 @@
 class Solution {
     public boolean solution(int x) {
-        boolean answer = true;
-        String[] str = String.valueOf(x).split("");
-        int sum = 0;
-        for(int i = 0; i < str.length;i++){
-            sum += Integer.parseInt(str[i]);
-        }
-        if(x % sum != 0) return false;
-        return answer;
+       int sum = String.valueOf(x).chars().map(ch -> ch - '0').sum();
+        return x % sum == 0;
     }
 }
