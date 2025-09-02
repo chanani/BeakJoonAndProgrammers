a4719613a228dd0c8e14ef32fe5bae9f6acee548
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-09-02 21:29:41 +0900

    [level 0] Title: 두 수의 합, Time: 453.26 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -1,10 +1,6 @@
 import java.math.*;
 class Solution {
     public String solution(String a, String b) {
-        BigInteger i = new BigInteger(a);
-        BigInteger j = new BigInteger(b);
-
-        return i.add(j).toString();
-
+        return new BigInteger(a).add(new BigInteger(b)).toString();
     }
 }
