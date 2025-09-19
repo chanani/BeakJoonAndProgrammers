fc182ba2d3bb3f9e991cb21d2cbd6859873e0c21
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-09-19 22:22:23 +0900

    [level 0] Title: 할 일 목록, Time: 2.20 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -1,13 +1,11 @@
+import java.util.stream.*;
 import java.util.*;
 class Solution {
     public ArrayList<String> solution(String[] todo_list, boolean[] finished) {
-        ArrayList<String> list = new ArrayList<>();
-        for(int i = 0; i < finished.length; i++){
-            if(!finished[i]){
-                list.add(todo_list[i]);
-            }
-        }
-        
-        return list;
+        ArrayList<String> answer = new ArrayList<>();
+        IntStream.range(0, todo_list.length).forEach(i -> {
+            if(!finished[i]) answer.add(todo_list[i]);
+        });
+        return answer;
     }
 }
