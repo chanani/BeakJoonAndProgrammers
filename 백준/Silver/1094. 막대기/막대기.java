c2b6b8000d785351e10304e32073c8e444ba9364
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-10-25 16:06:20 +0900

    [Silver V] Title: 막대기, Time: 164 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -1,30 +1,16 @@
-
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int X = scanner.nextInt();
-        int[] baseArr = {64, 32, 16, 8, 4, 2, 1};
         int count = 0;
 
-        for (int i = 0; i < baseArr.length; i++) {
-            if(baseArr[i] == X){
-                System.out.print(1);
-                return;
-            }
-        }
-
-        int sum = 0;
-        for (int i = 0; i < baseArr.length; i++) {
-            int temp = sum + baseArr[i];
-            if (temp < X){
+        while(X > 0){
+            if(X % 2 == 1){
                 count++;
-                sum = temp;
-            } else if(temp == X){
-                count++;
-                break;
             }
+            X = X / 2;
         }
 
         System.out.print(count);
