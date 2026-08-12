c6c6dd34e2bce8daf770e97c60cad6c3e6dba9a2
CHANHAN <theholidaynight@gmail.com>
2026-08-13 08:34:38 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.98 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,44 +1,42 @@
 import java.util.*;
 
-class Point{
-    public int x, y;
-    Point(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-}
-
 class Solution {
-    static int[][] visit;
-    static int[][] board;
-    static int n, m;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    public int solution(int[][] maps) {
-        n = maps.length;
-        m = maps[0].length;
-        visit = new int[n][m];
-        BFS(0, 0, maps);
-        if(visit[n - 1][m- 1] == 0) return -1;
-        else return visit[n- 1][m- 1];
-    }
+    int n;
+    int m;
+    int[] X = {1, -1, 0, 0};
+    int[] Y = {0, 0, 1, -1};
     
-    public static void BFS(int x, int y, int[][] maps){
-        Queue<Point> Q = new LinkedList<>();
-        Q.offer(new Point(x, y));
-        visit[x][y] = 1;
-        while(!Q.isEmpty()){
-            Point point = Q.poll();
-            for(int i = 0; i < 4; i++){
-                int nx = point.x + dx[i];
-                int ny = point.y + dy[i];
-                if(nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] == 1){
-                    maps[nx][ny] = 0;
-                    Q.offer(new Point(nx, ny));
-                    visit[nx][ny] = visit[point.x][point.y] + 1;
+    public int solution(int[][] maps) {
+        int answer = 1;
+        this.n = maps.length;
+        this.m = maps[0].length;
+        boolean[][] visited = new boolean[n][m];
+        Deque<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{0, 0, 1});
+        visited[0][0] = true;
+        
+        while(!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int count = cur[2];
+            
+            if(cur[0] == n - 1 && cur[1] == m - 1) {
+                return count;
+            }
+            
+            for(int i = 0; i < 4; i++) {
+                int nx = cur[0] + X[i];
+                int ny = cur[1] + Y[i];
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m){
+                    if(!visited[nx][ny] && maps[nx][ny] == 1){
+                        queue.offer(new int[]{nx, ny, count + 1});
+                        visited[nx][ny] = true;
+                    }
                 }
+                
             }
         }
         
+        
+        return -1;
     }
 }
