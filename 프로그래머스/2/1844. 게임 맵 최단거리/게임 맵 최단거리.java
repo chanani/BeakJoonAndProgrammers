b0808c05954bd0f4a07240c5b0b657575ada6f2f
CHANHAN <theholidaynight@gmail.com>
2026-08-24 08:46:38 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.82 ms, Memory: 57.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,42 +1,42 @@
 import java.util.*;
 
 class Solution {
-    int n;
-    int m;
-    int[] X = {1, -1, 0, 0};
-    int[] Y = {0, 0, 1, -1};
+    private final int[] X = {1, -1, 0, 0};
+    private final int[] Y = {0, 0, 1, -1};
     
     public int solution(int[][] maps) {
-        int answer = 1;
-        this.n = maps.length;
-        this.m = maps[0].length;
+        int answer = -1;
+        int n = maps.length;
+        int m = maps[0].length;
+        
+        Queue<int[]> queue = new ArrayDeque<>();
         boolean[][] visited = new boolean[n][m];
-        Deque<int[]> queue = new ArrayDeque<>();
         queue.offer(new int[]{0, 0, 1});
         visited[0][0] = true;
         
-        while(!queue.isEmpty()) {
+        while(!queue.isEmpty()){
             int[] cur = queue.poll();
+            int cx = cur[0];
+            int cy = cur[1];
             int count = cur[2];
             
-            if(cur[0] == n - 1 && cur[1] == m - 1) {
+            if(cx == n - 1 && cy == m - 1){
                 return count;
             }
             
             for(int i = 0; i < 4; i++) {
-                int nx = cur[0] + X[i];
-                int ny = cur[1] + Y[i];
-                if(nx >= 0 && nx < n && ny >= 0 && ny < m){
-                    if(!visited[nx][ny] && maps[nx][ny] == 1){
+                int nx = cx + X[i];
+                int ny = cy + Y[i];
+                
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m) {
+                    if(!visited[nx][ny] && maps[nx][ny] == 1) {
                         queue.offer(new int[]{nx, ny, count + 1});
                         visited[nx][ny] = true;
                     }
                 }
-                
             }
         }
         
-        
-        return -1;
+        return answer;
     }
 }
